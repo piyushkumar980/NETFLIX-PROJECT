3f0f48ed57a76d56764759193d29b6f7341dcb72
piyush kumar <147308217+piyushkumar980@users.noreply.github.com>
2024-07-30 00:56:41 +0530

    Add files via upload

---
+++ netflix4.js
@@ -0,0 +1,24 @@
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/netflix%20clone/netflix5.html";
+});
+
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", () => {
+    document
+      .querySelectorAll(".card")
+      .forEach((c) => c.classList.remove("shadow"));
+    card.classList.add("shadow");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      cards.forEach((c) => c.classList.remove("selected"));
+      card.classList.add("selected");
+    });
+  });
+});
